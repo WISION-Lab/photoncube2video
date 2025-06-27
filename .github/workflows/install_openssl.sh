a4b740b73f97826215540bebd8c863272017e75a
Sacha Jungerman <jungerm2@illinois.edu>
2025-06-26 21:42:10 -0500

    missing envvars

--- .github/workflows/install_openssl.sh
+++ .github/workflows/install_openssl.sh
@@ -1,3 +1,4 @@
+# Install OpenSSL
 if command -v yum &> /dev/null; then
     # If we're running on rhel centos, install needed packages.
     yum update -y && yum install -y perl-core openssl openssl-devel pkgconfig libatomic
@@ -5,3 +6,8 @@ else
     # If we're running on debian-based system.
     apt update -y && apt-get install -y libssl-dev openssl pkg-config
 fi
+
+# Missing linker envvars
+export HOST_CC=gcc
+export CC_x86_64_unknown_linux_gnu=/usr/bin/x86_64-linux-gnu-gcc
+export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=/usr/bin/x86_64-linux-gnu-gcc
