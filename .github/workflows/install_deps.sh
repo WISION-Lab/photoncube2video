dac5a122205ac1863797244ad0ca8881f4345273
Sacha Jungerman <jungerm2@illinois.edu>
2025-06-27 08:56:14 -0500

    permission issues 2

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -1,11 +1,13 @@
 # Install OpenSSL
 if command -v yum &> /dev/null; then
     # If we're running on rhel centos, install needed packages.
-    sudo yum update -y && yum install -y perl-core openssl openssl-devel pkgconfig libatomic
+    sudo yum update -y 
+    sudo yum install -y perl-core openssl openssl-devel pkgconfig libatomic
     sudo yum groupinstall 'Development Tools'
 else
     # If we're running on debian-based system.
-    sudo apt update -y && apt-get install -y libssl-dev openssl pkg-config build-essential
+    sudo apt update -y 
+    sudo apt-get install -y libssl-dev openssl pkg-config build-essential
 fi
 
 # Use zig as the linker to avoid common linking issues
